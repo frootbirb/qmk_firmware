a3089885e4691f0c89b884e04e62bff5f10d69e3
frootbirb <frootbirb@gmail.com>
2021-03-24 19:37:19 -0400

    Adding support for my handwired Dactyl

--- keyboards/handwired/dactyl_promicro/config.h
+++ keyboards/handwired/dactyl_promicro/config.h
@@ -27,10 +27,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 6
 
 // wiring of each half
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
-#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6 }
+#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4, B5 }
 
-#define DIODE_DIRECTION COL2ROW
+#define DIODE_DIRECTION ROW2COL
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
---
+++ keyboards/handwired/dactyl_promicro/keymaps/frootbirb/keymap.c
@@ -0,0 +1,29 @@
+#include QMK_KEYBOARD_H
+#include "frootbirb.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [DEF] = LAYOUT_6x6(
+		xxxx,    KC_1,  KC_2,  KC_3, KC_4, KC_5,                                         KC_6,   KC_7, KC_8,    KC_9,    KC_0,    xxxx,
+		KC_ESC,  KC_Q,  KC_W,  KC_E, KC_R, KC_T,                                         KC_Y,   KC_U, KC_I,    KC_O,    KC_P,    FIND,
+		KC_TAB,  KC_A,  KC_S,  KC_D, KC_F, KC_G,                                         KC_H,   KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+		KC_LSFT, KC_Z,  KC_X,  KC_C, KC_V, KC_B,   xxxx,    xxxx,       xxxx,    xxxx,   KC_N,   KC_M, KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,
+		MOD_1, 	 MOD_2, MOD_3, COPY, xxxx,                  MO(NAV),    MO(NAV),                 xxxx, xxxx,    KC_LEFT, KC_DOWN, KC_RGHT,
+		                                   KC_SPC, SFT_ENT, MO(SYM),    MO(SYM), KC_SPC, KC_BSPC
+    ),
+    [SYM] = LAYOUT_6x6(
+		RESET, ____,    ____,    ____,    ____,    ____,                                    ____,    ____,    ____,    ____,    ____,    ____,
+		____,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                                    ____,    KC_UNDS, KC_AT,   KC_HASH, KC_DLR,  ____,
+		____,  KC_TILD, KC_QUES, KC_EXLM, KC_SLSH, KC_AMPR,                                 KC_PIPE, PAREN,   BRACE,   BRACK,   ABRCK,   KC_GRV,
+		____,  KC_PLUS, KC_MINS, KC_ASTR, KC_BSLS, KC_EQL, ____, ____,    ____, ____, ____, ____,    KC_CIRC, KC_PERC, ____,    ____,    ____,
+		____,  ____,    ____,    ____,                           ____,    ____,                      ____,    ____,    ____,    ____,    ____,
+		                                           ____,   ____, ____,    ____, ____, ____
+    ),
+    [NAV] = LAYOUT_6x6(
+		KC_PWR, ____,  ____,   ____,   ____,   ____,                                   ____, ____,    RGB_HUD, RGB_HUI, ____,    PASSWD,
+		____,   KC_F1, KC_F2,  KC_F3,  KC_F4,  ____,                                   ____, RGB_TOG, RGB_VAD, RGB_VAI, RGB_MOD, SYS_FND,
+		____,   KC_F5, KC_F6,  KC_F7,  KC_F8,  ____,                                   ____, KC_INS,  KC_NLCK, KC_CAPS, KC_SLCK, ____,
+		____,   KC_F9, KC_F10, KC_F11, KC_F12, ____, ____, ____,    ____, ____, ____,  ____, ____,    ____,    SCRNSHT, KC_PGUP, KC_CAPS,
+		MOD_4,  MOD_5, MOD_6,  ____,                       ____,    ____,                    ____,    TOG_OS,  KC_HOME, KC_PGDN, KC_END,
+		                                       ____, ____, ____,    ____, ____, KC_DEL
+    ),
+};
